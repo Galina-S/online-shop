3596daa0134b4ca69c443f5e55087f1cfa712a18
Galina S <98215274+Galina-S@users.noreply.github.com>
2022-11-29 10:32:02 +0100

    Update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -10,7 +10,7 @@ export function Store () {
             <Row md= {2}  xs ={1} lg= {3} className="g-3">
                 {storeItems.map(item => (
                  <Col key = {item.id}>
-                    <StoreItem brand={""} {...item}/>
+                    <StoreItem {...item}/>
                 </Col>
                 )
                 )}
